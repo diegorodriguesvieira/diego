ad29d02b737ece7f7d59989d25ba82f22061c03c
Diego Rodrigues Vieira <diegorodrigues92@gmail.com>
2021-02-04 22:39:34 -0300

    remove unnecessary files

--- src/components/Icon/Icon.d.ts
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import './Icon.scss';
-export declare type IconNames = 'add' | 'add-circle';
-export interface IconProps {
-    name: IconNames;
-}
-declare const Icon: React.FC<IconProps>;
-export default Icon;
