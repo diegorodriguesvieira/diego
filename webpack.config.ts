49a62fb4167ed9eb2475bc68b8dc982bbc96a25f
Diego Rodrigues Vieira <diegorodrigues92@gmail.com>
2021-02-05 00:26:59 -0300

    move ts types config from webpack to tsconfig

--- webpack.config.ts
+++ webpack.config.ts
@@ -14,12 +14,7 @@ module.exports = {
       {
         test: /\.tsx?$/,
         loader: 'ts-loader',
-        exclude: /node_modules/,
-        options: {
-          compilerOptions: {
-            declarationDir: path.resolve(__dirname, 'dist/types')
-          }
-        }
+        exclude: /node_modules/
       },
       {
         test: /\.s[ac]ss$/i,
